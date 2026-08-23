9fea1cd99c49e2b6e932fdf105989aef3fb7849a
Dimitar Vlahov <dimitarvlahov24@gmail.com>
2026-08-24 01:02:12 +0300

    .

--- configs/hypr/hyprland.lua
+++ configs/hypr/hyprland.lua
@@ -2,30 +2,27 @@
 --  MONITORS  --
 ----------------
 -- hl.monitor({
---     output = "DP-1",
---     mode = "3440x1440@180",
---     position = "0x0",
+--     output = "DP-2",
+--     mode = "1920x1080@75",
+--     position = "-1920x0",
 --     scale = 1
 -- })
+
+-- Turbo-X Nemesis 49" - TOP
 hl.monitor({
     output = "DP-2",
-    mode = "1920x1080@75",
-    position = "-1920x0",
-    scale = 1
-})
-
-hl.monitor({
-    output = "DP-1",
     mode = "5120x1440@120",
-    position = "0x0",
+    position = "0x-1440",
     scale = 1,
 })
--- hl.monitor({
---     output = "DP-1",
---     mode = "3440x1440@180",
---     position = "840x-1440",
---     scale = 1
--- })
+
+-- AOC 34" - BOTTOM
+hl.monitor({
+    output = "DP-1",
+    mode = "3440x1440@180",
+    position = "840x0",
+    scale = 1,
+})
 
 -------------------
 --  MY PROGRAMS  --
